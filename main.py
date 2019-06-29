cb490be4a3dd43f35e27b2819cb1b48c62db67f0
Fatih Akturk <fatih.akturk@bulutyazilim.com>
2019-06-29 19:17:33 +0300

    we get article title, author and link from startupschool page for each article and add them in a dictionary with related upper title

--- main.py
+++ main.py
@@ -12,11 +12,28 @@ soup = BeautifulSoup(page.text, 'html.parser')
 # print(soup.prettify())
 # soup.find_all(class_='piece-title')
 titles = soup.find_all(class_='title')
+articles={}
 # print(titles)
 for t in titles:
     # print(t)
     span = t.span.text
     print(span)
+    content=t.find_next_sibling(class_="content") 
+    for c in content:
+        pieceLink = c['href']
+        pieceTitle = c.find(class_='piece-title').text
+        pieceAuthor = c.find(class_='piece-author').text
+        # print(pieceTitle,pieceAuthor,pieceLink)
+        piece = [pieceTitle,pieceAuthor,pieceLink]
+        # print(c.prettify())
+        # piece = soup.find_all(class_='piece')
+        # # print(c)
+        # for p in piece:
+        #     print(p.text)
+        
+    # print(content)
+    articles[span]=piece
+print(articles)
 # tree = html.fromstring(r.content)
 # print(tree)
 #This will create a list of titles:
